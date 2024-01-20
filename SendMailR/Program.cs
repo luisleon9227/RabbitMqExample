829f652832fa0254675384212f6c99cc36f62a38
Luis Alfredo Leon Britto <luis.leon+Siigo@siigo.com>
2024-01-20 12:10:21 -0500

    Example rabbit

---
+++ SendMailR/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = "admin",
+    Password = "admin"
+};
+
+try
+{
+    var connection = factory.CreateConnection();
+    using
+    var channel = connection.CreateModel();
+    channel.QueueDeclare("ejemplo", exclusive: false);
+    var consumer = new EventingBasicConsumer(channel);
+
+    consumer.Received += (model, EventArgs) =>
+    {
+        var body = EventArgs.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine("Mensaje recibido: " + message);
+    };
+    channel.BasicConsume("ejemplo", true, consumer);
+    Console.ReadKey();
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Error leyendo el mensaje " + ex.Message);
+}
